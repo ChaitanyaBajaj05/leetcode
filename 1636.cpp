ac321a6195ca9d954b81f363591c756723c6f7d5
Chaitanya bajaj <chaitanyabajaj42@gmail.com>
2025-12-18 11:34:48 +0530

    Initial commit - leetcode solutions

---
+++ 1636.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        int n = nums.size();
+        int freq[201] = {0};
+        for (int i = 0; i < n; i++) {
+            freq[nums[i] + 100]++;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (freq[nums[j] + 100] > freq[nums[j + 1] + 100] ||
+                   (freq[nums[j] + 100] == freq[nums[j + 1] + 100] &&
+                    nums[j] < nums[j + 1])) {
+
+                    swap(nums[j], nums[j + 1]);
+                }
+            }
+        }
+
+        return nums;
+    }
+};
